9764d665bc5ebf7f230c347478a8f954cf28063d
Rio0912 <rehanansari0912@gmail.com>
2024-05-08 22:06:57 +0530

    changes completed

--- server.js
+++ server.js
@@ -3,38 +3,27 @@ const mongoose = require("mongoose")
 // const user = require("./Models/Usermodel")
 const productrouter = require("./Routers/ProductRouter");
 const dotenv = require('dotenv');
-<<<<<<< HEAD
 const auth = require("./Routers/AuthRouter")
 const orders = require("./Routers/OrderRoute")
 const payment = require("./Routers/PaymentRoute")
 
 const app = express();
 
-=======
 const cors =require("cors")
-const auth = require("./Routers/AuthRouter")
-const orders = require("./Routers/OrderRoute")
 const category=require("./Routers/CategoryRouter")
 const cart =require("./Routers/CartRouter")
-const app = express();
+
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-<<<<<<< HEAD
 
 
->>>>>>> ec5c9fc65548fa6da49a4d4f0f1969f2c52c1a91
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-<<<<<<< HEAD
-=======
-=======
->>>>>>> b92fa63bc7c2b4f0558fdf4c9cbf870f19a980dd
->>>>>>> ec5c9fc65548fa6da49a4d4f0f1969f2c52c1a91
 
 dotenv.config();
 app.use(express.json({ limit: '100mb' }));
@@ -46,12 +35,12 @@ app.use(cors({
 app.use("/admin", productrouter)
 app.use("/auth/", auth)
 app.use("/order/", orders)
-<<<<<<< HEAD
+
 app.use("/pay", payment)
-=======
+
 app.use("/admin/category",category )
 app.use("/cart/", cart)
->>>>>>> ec5c9fc65548fa6da49a4d4f0f1969f2c52c1a91
+
 
 
 
@@ -69,7 +58,6 @@ mongoose.connect(mongoDB)
 
 
 
-<<<<<<< HEAD
 app.post("/Image", async (req, res) => {
   try {
     const result = await cloudinary.uploader.upload(req.body.image);
@@ -84,14 +72,6 @@ app.post("/Image", async (req, res) => {
   }
 });
 const PORT = process.env.PORT || 9000;
-=======
-
-<<<<<<< HEAD
-const PORT = process.env.PORT || 9000;
-=======
-const PORT = process.env.PORT || 5000;
->>>>>>> b92fa63bc7c2b4f0558fdf4c9cbf870f19a980dd
->>>>>>> ec5c9fc65548fa6da49a4d4f0f1969f2c52c1a91
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
