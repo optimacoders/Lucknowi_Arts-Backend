4752f301537647ef6f25237494770bce05ae8334
OptimaCoders <167647381+optimacoders@users.noreply.github.com>
2024-05-26 01:58:21 +0530

    done

--- Routers/OrderRoute.js
+++ Routers/OrderRoute.js
@@ -2,11 +2,12 @@
 
 const express = require('express');
 const router = express.Router();
-const { createOrder, updateOrderStatus, getOrdersByUserId, getAllOrders } = require('../Controllers/OrderController');
+const { createOrder, updateOrderStatus, getOrdersByUserId, getAllOrders, getmyOrder } = require('../Controllers/OrderController');
 const verifyToken = require('../Middleware/Authenticating');
 
 router.post('/create',verifyToken, createOrder);
 router.get('/myorders',verifyToken, getOrdersByUserId);
+router.get('/myorder/:orderid',verifyToken, getmyOrder);
 
 
 module.exports = router;
