9d30eb895a2769020124fd580696df71c7aad84b
OptimaCoders <167647381+optimacoders@users.noreply.github.com>
2024-05-25 16:03:26 +0530

    done

--- Routers/ProductRouter.js
+++ Routers/ProductRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { addproduct, getProducts, getProductById } = require('../Controllers/ProductController');
+const { addproduct, getProducts, getProductById, getSimilarProducts } = require('../Controllers/ProductController');
 const verifyToken = require('../Middleware/Authenticating');
 const router = express.Router();
 
 router.post('/', addproduct);
 router.get('/', getProducts);
 router.get('/:id', getProductById);
+router.get('/getsimilarproducts/:category', getSimilarProducts);
 
 
 module.exports = router
